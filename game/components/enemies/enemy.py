ae23a7f4e349fda6b622a1e836be77b07a07ecd2
bsqmAcademlo <brayan.munoz@academlo.com>
2023-07-14 19:38:44 -0500

    class 2, add to enemies and handles to this

---
+++ game/components/enemies/enemy.py
@@ -0,0 +1,50 @@
+import random
+from game.utils.constants import SCREEN_WIDTH, SCREEN_HEIGHT, LEFT, RIGHT
+
+
+class Enemy:
+    Y_POS = 0
+    SPEED_X = 5
+    SPEED_Y = 2
+    MOVE_X = [LEFT, RIGHT]
+    INTERVAL = 100
+
+    def __init__(self, image):
+        self.image = image
+        self.rect = self.image.get_rect()
+        self.rect.x = random.randint(
+            image.get_width(), SCREEN_WIDTH - image.get_width()
+        )
+        self.rect.y = self.Y_POS
+        self.move_x = random.choice(self.MOVE_X)
+        self.index = 0
+        self.is_visible = True
+
+    def update(self):
+        self.move()
+
+        if self.rect.y > SCREEN_HEIGHT:
+            self.is_visible = False
+
+        self.index += 1
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+
+    def move(self):
+        self.rect.y += self.SPEED_Y
+        if self.move_x == LEFT:
+            self.rect.x -= self.SPEED_X
+
+            if self.rect.x <= 0 or self.index > self.INTERVAL:
+                self.move_x = RIGHT
+                self.index = 0
+        else:
+            self.rect.x += self.SPEED_X
+
+            if (
+                self.rect.x >= SCREEN_WIDTH - self.image.get_width()
+                or self.index > self.INTERVAL
+            ):
+                self.move_x = LEFT
+                self.index = 0
--- game/utils/constants.py
+++ game/utils/constants.py
@@ -11,20 +11,25 @@ IMG_DIR = os.path.join(os.path.dirname(__file__), "..", "assets")
 # Assets Constants
 ICON = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
 
-SHIELD = pygame.image.load(os.path.join(IMG_DIR, 'Other/shield.png'))
+SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Other/shield.png"))
 
-BG = pygame.image.load(os.path.join(IMG_DIR, 'Other/Track.png'))
+BG = pygame.image.load(os.path.join(IMG_DIR, "Other/Track.png"))
 
-HEART = pygame.image.load(os.path.join(IMG_DIR, 'Other/SmallHeart.png'))
+HEART = pygame.image.load(os.path.join(IMG_DIR, "Other/SmallHeart.png"))
 
 DEFAULT_TYPE = "default"
-SHIELD_TYPE = 'shield'
+SHIELD_TYPE = "shield"
 
 SPACESHIP = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship.png"))
-SPACESHIP_SHIELD = pygame.image.load(os.path.join(IMG_DIR, "Spaceship/spaceship_shield.png"))
+SPACESHIP_SHIELD = pygame.image.load(
+    os.path.join(IMG_DIR, "Spaceship/spaceship_shield.png")
+)
 BULLET = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_1.png"))
 
 BULLET_ENEMY = pygame.image.load(os.path.join(IMG_DIR, "Bullet/bullet_2.png"))
 ENEMY_1 = pygame.image.load(os.path.join(IMG_DIR, "Enemy/enemy_1.png"))
 
-FONT_STYLE = 'freesansbold.ttf'
+FONT_STYLE = "freesansbold.ttf"
+
+LEFT = "left"
+RIGHT = "right"
